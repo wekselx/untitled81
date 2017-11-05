ae742c2b934f0e1c67d21469fbaa2943923be42e
wekselx <wlad.we@yandex.ru>
2017-11-06 01:28:51 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import processing.core.PApplet;
+
+public class Main {
+    private final static String[] OPTIONS=new String[]{"--present","com.company.Sketch"};
+
+    public static void main(String[] args) {
+        PApplet.main(OPTIONS);
+
+    }
+}
